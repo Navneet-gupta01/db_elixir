5222f053323d5efb10c251a3fd1ad77f3739cab5
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-05-26 08:29:05 +0400

    Added rest api for bar db dest table

--- apps/dbs/lib/dbs/web.ex
+++ apps/dbs/lib/dbs/web.ex
@@ -16,20 +16,50 @@ defmodule Dbs.Web do
   get("/", do: send_resp(conn, 200, "Great Router Configured"))
 
   get("/dbs/foo/tables/source") do
+    conn = Plug.Conn.fetch_query_params(conn)
+
+    from_id =
+      conn
+      |> Plug.Conn.fetch_query_params(conn)
+      |> (fn conn -> conn.params end).()
+      |> Map.get("from_id", 0)
+
+    IO.puts("got from_id from query params : #{from_id}")
+
     conn =
       conn
       |> put_resp_content_type("text/event-stream")
       |> send_chunked(200)
 
     header = "a,b,c"
-    conn |> chunk("data: #{header}\n\n")
+    conn |> chunk("#{header}\n")
 
-    send_next(conn, 999_000)
+    send_next_foo(conn, from_id)
   end
 
-  def send_next(conn, from_id) do
-    IO.puts("Calling send next from id : #{from_id}")
+  get("/dbs/bar/tables/dest") do
+    conn = Plug.Conn.fetch_query_params(conn)
 
+    from_id =
+      conn
+      |> Plug.Conn.fetch_query_params(conn)
+      |> (fn conn -> conn.params end).()
+      |> Map.get("from_id", 0)
+
+    IO.puts("got from_id from query params : #{from_id}")
+
+    conn =
+      conn
+      |> put_resp_content_type("text/event-stream")
+      |> send_chunked(200)
+
+    header = "a,b,c"
+    conn |> chunk("#{header}\n")
+
+    send_next_bar(conn, from_id)
+  end
+
+  def send_next_foo(conn, from_id) do
     %Postgrex.Result{rows: rows, num_rows: num_rows} =
       Postgrex.query!(
         Application.get_env(:dbs, :foo_database_conf)[:name],
@@ -39,7 +69,6 @@ defmodule Dbs.Web do
 
     cond do
       num_rows > 0 ->
-        IO.puts("Number of row fetched #{num_rows}")
         [last_index, _b, _c] = List.last(rows)
 
         conn =
@@ -53,12 +82,42 @@ defmodule Dbs.Web do
             end
           end)
 
-        send_next(conn, last_index)
+        send_next_foo(conn, last_index)
 
       true ->
-        IO.puts("else condition")
-        :timer.sleep(1_000)
-        send_next(conn, from_id)
+        :timer.sleep(2_000)
+        send_next_foo(conn, from_id)
+    end
+  end
+
+  def send_next_bar(conn, from_id) do
+    %Postgrex.Result{rows: rows, num_rows: num_rows} =
+      Postgrex.query!(
+        Application.get_env(:dbs, :bar_database_conf)[:name],
+        "SELECT a, b, c FROM dest order by a limit 10000 offset #{from_id}",
+        []
+      )
+
+    cond do
+      num_rows > 0 ->
+        [last_index, _b, _c] = List.last(rows)
+
+        conn =
+          Enum.reduce_while(rows, conn, fn [a, b, c], conn ->
+            case chunk(conn, "data: #{a}, #{b}, #{c}\n\n") do
+              {:ok, conn} ->
+                {:cont, conn}
+
+              _ ->
+                {:halt, conn}
+            end
+          end)
+
+        send_next_bar(conn, last_index)
+
+      true ->
+        :timer.sleep(2_000)
+        send_next_bar(conn, from_id)
     end
   end
 
