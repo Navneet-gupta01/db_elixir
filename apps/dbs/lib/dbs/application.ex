a845b386d038ff485fe0ca19d0d12de7607374b8
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-05-26 11:00:21 +0400

    clean up

--- apps/dbs/lib/dbs/application.ex
+++ apps/dbs/lib/dbs/application.ex
@@ -2,14 +2,6 @@ defmodule Dbs.Application do
   use Application
 
   def start(_, _) do
-    # import Supervisor.Spec
-    #
-    # children = [
-    #   supervisor(Dbs.Supervisor, [])
-    # ]
-    #
-    # opts = [strategy: :one_for_one, name: __MODULE__]
-    # Supervisor.start_link(children, opts)
     Dbs.Supervisor.start_link()
   end
 end
