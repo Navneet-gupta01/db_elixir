e98216c2fa24051d0fe661e0c31f9a9e0a41fc12
Navneet <navneet.gupta.iitbhu@gmail.com>
2019-05-28 13:28:43 +0400

    changes

--- apps/dbs/lib/dbs/web.ex
+++ apps/dbs/lib/dbs/web.ex
@@ -16,6 +16,17 @@ defmodule Dbs.Web do
   get("/", do: send_resp(conn, 200, "Great Router Configured"))
 
   get("/dbs/foo/tables/source") do
+    conn = Plug.Conn.fetch_query_params(conn)
+
+    {from_id, _} =
+      conn
+      |> Plug.Conn.fetch_query_params(conn)
+      |> (fn conn -> conn.params end).()
+      |> Map.get("from_id", "0")
+      |> Integer.parse()
+
+    IO.puts("got from_id from query params : #{from_id}")
+
     conn =
       conn
       |> put_resp_content_type("text/event-stream")
@@ -24,10 +35,21 @@ defmodule Dbs.Web do
     header = "a,b,c"
     conn |> chunk("#{header}\n")
 
-    send_next_foo(conn)
+    send_next_foo(conn, from_id)
   end
 
   get("/dbs/bar/tables/dest") do
+    conn = Plug.Conn.fetch_query_params(conn)
+
+    {from_id, _} =
+      conn
+      |> Plug.Conn.fetch_query_params(conn)
+      |> (fn conn -> conn.params end).()
+      |> Map.get("from_id", "0")
+      |> Integer.parse()
+
+    IO.puts("got from_id from query params : #{from_id}")
+
     conn =
       conn
       |> put_resp_content_type("text/event-stream")
@@ -36,45 +58,61 @@ defmodule Dbs.Web do
     header = "a, b, c"
     conn |> chunk("#{header}\n")
 
-    send_next_bar(conn)
+    send_next_bar(conn, from_id)
   end
 
-  def send_next_foo(conn) do
-    Postgrex.transaction(
-      Application.get_env(:dbs, :foo_database_conf)[:name],
-      fn pconn ->
-        Postgrex.stream(
-          pconn,
-          "SELECT a, b, c FROM source order by a",
-          [],
-          max_rows: 1
-        )
-        |> Stream.map(fn %Postgrex.Result{rows: rows} ->
-          Enum.reduce_while(rows, conn, fn [a, b, c], conn ->
-            case chunk(conn, "#{a}, #{b}, #{c}\n") do
-              {:ok, conn} ->
-                {:cont, conn}
+  def send_next_foo(conn, from_id) do
+    try do
+      Postgrex.transaction(
+        Application.get_env(:dbs, :foo_database_conf)[:name],
+        fn pconn ->
+          IO.inspect("conn")
+
+          Postgrex.stream(
+            pconn,
+            "SELECT a, b, c FROM source order by a offset #{from_id}",
+            [],
+            max_rows: 1,
+            timeout: :infinity
+          )
+          |> Stream.map(fn %Postgrex.Result{rows: rows} ->
+            case rows do
+              [_ | _] ->
+                Enum.reduce_while(rows, conn, fn [a, b, c], conn ->
+                  case chunk(conn, "#{a}, #{b}, #{c}\n") do
+                    {:ok, conn} ->
+                      {:cont, conn}
+
+                    _ ->
+                      {:halt, conn}
+                  end
+                end)
 
               _ ->
-                {:halt, conn}
+                IO.inspect("test")
+                # send_next_foo(conn, arg2)
             end
           end)
-        end)
-        |> Stream.run()
-      end,
-      timeout: :infinity
-    )
+          |> Stream.run()
+        end,
+        timeout: :infinity
+      )
+    catch
+      x ->
+        IO.inspect(x)
+    end
   end
 
-  def send_next_bar(conn) do
+  def send_next_bar(conn, from_id) do
     Postgrex.transaction(
       Application.get_env(:dbs, :bar_database_conf)[:name],
       fn pconn ->
         Postgrex.stream(
           pconn,
-          "SELECT a, b, c FROM dest order by a",
+          "SELECT a, b, c FROM dest order by a offset #{from_id}",
           [],
-          max_rows: 1
+          max_rows: 1,
+          timeout: :infinity
         )
         |> Stream.map(fn %Postgrex.Result{rows: rows} ->
           Enum.reduce_while(rows, conn, fn [a, b, c], conn ->
